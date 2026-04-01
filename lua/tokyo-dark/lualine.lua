5500270c06268a8aa2fb10c9344d05fb95631c3c
Ayush Kumar <ayush438500@gmail.com>
2026-04-01 17:44:50 +0530

    ADD support for lualine

---
+++ lua/tokyo-dark/lualine.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+function M.theme(palette)
+  return {
+    normal = {
+      a = { fg = palette.bg, bg = palette.magenta, gui = "bold" },
+      b = { fg = palette.fg, bg = palette.ui.bg_statusline or palette.bg },
+      c = { fg = palette.fg, bg = palette.bg },
+    },
+    insert = {
+      a = { fg = palette.bg, bg = palette.green1, gui = "bold" },
+    },
+    visual = {
+      a = { fg = palette.bg, bg = palette.blue2, gui = "bold" },
+    },
+    replace = {
+      a = { fg = palette.bg, bg = palette.red1 or palette.magenta, gui = "bold" },
+    },
+    inactive = {
+      a = { fg = palette.comment, bg = palette.bg },
+      b = { fg = palette.comment, bg = palette.bg },
+      c = { fg = palette.comment, bg = palette.bg },
+    },
+  }
+end
+
+return M
